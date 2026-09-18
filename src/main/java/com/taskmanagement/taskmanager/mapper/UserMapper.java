07746e4d5425f1819f0c83b151523415871e9095
Pavlo Sukhanko <pashasyx@gmail.com>
2026-09-18 15:40:13 +0100

    configure MapStruct and add user mapper

---
+++ src/main/java/com/taskmanagement/taskmanager/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.taskmanagement.taskmanager.mapper;
+
+import com.taskmanagement.taskmanager.dto.user.UserRegistrationRequestDto;
+import com.taskmanagement.taskmanager.dto.user.UserResponseDto;
+import com.taskmanagement.taskmanager.models.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    User toEntity(UserRegistrationRequestDto requestDto);
+
+    UserResponseDto toDto(User user);
+}
